a7958474c16aee2d497fd79728747107f49b28a4
Brandon VanCamp <brandon@overtimewebdev.com>
2018-07-06 17:51:06 -0500

    Clean up

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 // // Use Node.js to perform look ups
 // //Create web server;
- // NOTE: New sever rendering output below.
 var scraper = require('./scraper.js');
 var http = require('http');
 var config = { // Config for server options
   portNumber: 3000,
 };
-
+// NOTE: New sever rendering output below.
 http.createServer(function (request, response) {
   scraper.shirt(request, response);
 }).listen(config.portNumber);
